793fb5fc2763cd761c8d52047d102944419c7a4f
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-01-20 10:16:26 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "aws-vpc-module" {
   source = "./modules/aws-vpc-module"
-  #source ="github.com/DaniyaDi/terraform-aws-vpc//modules/aws-vpc-module"
+  #source = "github.com/DaniyaDi/terraform-aws-vpc//modules/aws-vpc-module"
 }
